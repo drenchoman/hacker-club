f08e15ab15db5e551f2dca9d34fea3e90fa6becd
Oscar Harron <oscarharron@hotmail.com>
2025-05-13 17:36:03 +1200

    add urlstring env

--- db/pool.js
+++ db/pool.js
@@ -1,12 +1,9 @@
 const { Pool } = require('pg');
 
-const { PGHOST, PGUSER, PGDATABASE, PGPASSWORD, PGPORT } =
-  process.env;
+const { DBURL } = process.env;
+
+const connectionString = DBURL;
 
 module.exports = new Pool({
-  host: PGHOST,
-  user: PGUSER,
-  database: PGDATABASE,
-  password: PGPASSWORD,
-  port: PGPORT,
+  connectionString,
 });
